128628aa767c1d883e63e978b701dc3d12bafe15
asianParadissseee <asian.paradissseee@gmail.com>
2024-01-31 00:42:04 +0600

    delete all layout

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-   <Product title="Кроссы Пума" price="9500" />
-   <Product title="Кроссы Nike" price="10000" />
+
   </div>
 </template>
 
-<script setup>
-import Product from "@/components/product.vue";
+<script>
+export default {
 
-
+}
 </script>
 
 <style lang="scss" scoped>
--- src/components/product.vue
+++ src/components/product.vue
@@ -1,22 +1,12 @@
-<script setup>
-
-defineProps({
-  title: String,
-  price: Number
-})
-</script>
-
 <template>
-  <div class="product">
-    <h2>Товар: {{title}}</h2>
-    <h3>Цена: {{price}}</h3>
+  <div>
+
   </div>
 </template>
 
+<script setup>
+</script>
+
 <style scoped>
-.product {
-  border: 1px solid blue;
-  padding: 10px;
-  margin-bottom: 10px;
-}
+
 </style>
