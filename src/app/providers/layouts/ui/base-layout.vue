0ed30f3549d55fbccde8fc04994801a0994297a3
asianParadissseee <asian.paradissseee@gmail.com>
2024-01-31 02:34:41 +0600

    create sidebar

--- src/app/providers/layouts/ui/base-layout.vue
+++ src/app/providers/layouts/ui/base-layout.vue
@@ -1,10 +1,14 @@
 <template>
   <Navbar/>
-  <slot></slot>
+  <div class="flex">
+    <Sidebar/>
+    <slot></slot>
+  </div>
 </template>
 
 <script setup>
 import {Navbar} from "@/widgets/navbar/index.js";
+import {Sidebar} from "@/widgets/sidebar/index.js";
 </script>
 
 <style scoped>
---
+++ src/widgets/sidebar/index.js
@@ -0,0 +1,5 @@
+import Sidebar from "./ui/sidebar.vue"
+
+export {
+    Sidebar
+}
