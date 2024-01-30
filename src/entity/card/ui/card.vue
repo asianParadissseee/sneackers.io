b86feb40148cb9871a9a3e365414f599da225491
asianParadissseee <asian.paradissseee@gmail.com>
2024-01-31 02:22:31 +0600

    create sidebar component and finish card && card-list and get default props

--- src/entity/card/ui/card.vue
+++ src/entity/card/ui/card.vue
@@ -1,23 +1,35 @@
+<script setup>
+defineProps(
+    {
+      imageUrl: String,
+      title: String,
+      price: Number,
+      isFavorite: Boolean,
+      isAdded: Boolean,
+      onClickAdd: Function,
+      onClickFavorite: Function
+    }
+)
+</script>
 <template>
   <div
       class="relative  bg-white border border-slate-100 rounded-3xl p-10 cursor-pointer hover:-translate-y-2 transition hover:shadow-xl">
-    <img src="/like-1.svg" alt="like" class="absolute top-8 left-8"/>
-    <img src="/sneakers/sneakers-1.jpg" alt="sneakers"/>
+    <img @click="onClickFavorite" :src="isFavorite ? '/like-1.svg' :  '/like-2.svg'" alt="like"
+         class="absolute top-8 left-8"/>
+    <img :src="imageUrl" alt="sneakers"/>
     <p class="mt-2">
-      Мужские кроссовки Nike
+      {{ title }}
     </p>
     <div class="flex justify-between mt-5">
       <div class="flex flex-col">
         <span class="text-slate-500">Цена:</span>
-        <b>40 $</b>
+        <b>{{ price }} $</b>
       </div>
-      <img src="/plus.svg" alt="plus"/>
+      <img @click="onClickAdd" :src="isAdded ? `/plus.svg` : `/checked.svg` " alt="plus"/>
     </div>
   </div>
 </template>
 
-<script setup>
-</script>
 
 <style lang="scss" scoped>
 
--- src/widgets/card-list/ui/card-list.vue
+++ src/widgets/card-list/ui/card-list.vue
@@ -5,10 +5,10 @@ import {Card} from "@/entity/card/index.js";
 
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
+    <Card :image-url="`/sneakers/sneakers-1.jpg`" :title="`Мужские кроссы Nike`" :price="1000" :is-added="true"/>
+    <Card :image-url="`/sneakers/sneakers-2.jpg`" :title="`Мужские кроссы Puma`" :price="2000" :is-added/>
+    <Card :image-url="`/sneakers/sneakers-3.jpg`" :title="`Мужские кроссы Adidas`" :price="100000" />
+    <Card :image-url="`/sneakers/sneakers-4.jpg`" :title="`Мужские кроссы Lacoste`" :price="9999"/>
   </div>
 </template>
 
